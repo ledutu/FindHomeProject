649b199ba108ec6ce6589be2c81dfbb71e8ea186
ledutu <javajava159@gmail.com>
2019-09-12 16:11:59 +0700

    commit HomeFragment

--- app/src/main/java/com/example/findhomeproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/findhomeproject/ui/home/HomeFragment.java
@@ -1,50 +1,90 @@
 package com.example.findhomeproject.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.findhomeproject.R;
+import com.example.findhomeproject.adapter.MotelNewsAdapter;
+import com.example.findhomeproject.modelForMotel.MotelNews;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
-    EditText txtHomeA, txtHomeB;
-    TextView txtHomeResult;
-    Button btnHomeAdd;
+    ArrayList<MotelNews> arrMotelNews;
+    MotelNewsAdapter motelNewsAdapter;
+    ListView lvMotelNews;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-        txtHomeA = root.findViewById(R.id.txtHomeA);
-        txtHomeB = root.findViewById(R.id.txtHomeB);
-        txtHomeResult = root.findViewById(R.id.txtHomeResult);
-        btnHomeAdd = root.findViewById(R.id.btnHomeAdd);
-
-
-        btnHomeAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doAdd();
-            }
-        });
-
-        return root;
+        View home = inflater.inflate(R.layout.fragment_home, container, false);
+        lvMotelNews = home.findViewById(R.id.lvMotelNews);
+        arrMotelNews = new ArrayList<>();
+        motelNewsAdapter = new MotelNewsAdapter(getActivity(), R.layout.item_motel_news, arrMotelNews);
+        lvMotelNews.setAdapter(motelNewsAdapter);
+        addData();
+        return home;
     }
 
-    private void doAdd() {
-        int a = Integer.parseInt(txtHomeA.getText().toString());
-        int b = Integer.parseInt(txtHomeB.getText().toString());
-        int c = a+b;
-
-        txtHomeResult.setText("Result: "+c);
+    private void addData() {
+        arrMotelNews.add(new MotelNews("Phòng 1PN và 2PN tại Bành Văn Trân, Tân Bình",
+                "Phòng Cao Cấp Full Nội Thất 1PN và 2PN " +
+                        "NGAY GẦN NGÃ TƯ BẢY HIỀN " +
+                        "(149 Bành Văn Trân, P.7, Q.Tân Bình)" +
+                        "Phòng mới xây đang hoàn thiện nội thất ( Phòng Mới 100%), tháng 9 dọn vào ở. " +
+                        "Cho những ai thích căn phòng sang trọng ở khu vực trầm lắng yên bình.", R.drawable.ic_account_black_24px));
+        arrMotelNews.add(new MotelNews("Phòng Đủ tiện nghi QUẬN 7",
+                "Cần cho thuê phòng đẹp, đủ tiện nghi cơ bản xách vali vào là ở " +
+                        "1.Nhà rất đẹp và ngay mặt dường hẻm xe hơi " +
+                        "-WC riêng " +
+                        "2. Phòng trang bị đầy đủ tiện nghi, nước nóng năng lượng mặt trời, giường, tủ, máy lạnh, diện tích từ 20m2- 25m2 tuỳ lớn nhỏ"
+                        , R.drawable.ic_home_black_24dp));
+        arrMotelNews.add(new MotelNews("Hoàng hoa thám còn hải phòng to co ban công",
+                "Con hai phòng to co ban công " +
+                        "Giơ giấc tư do " +
+                        "Loi đi riêng " +
+                        "Khu an ninh sạch sẽ " +
+                        "Sat chợ hoang hoa thám " +
+                        "Co the ở do 3-5n " +
+                        "Uu tiên nhân viên van phòng.hộ gia đình " +
+                        "Ai co nhu câu liên hê kim",
+                        R.drawable.ic_dashboard_black_24dp));
+        arrMotelNews.add(new MotelNews("Phòng 1PN và 2PN tại Bành Văn Trân, Tân Bình",
+                "Phòng Cao Cấp Full Nội Thất 1PN và 2PN " +
+                        "NGAY GẦN NGÃ TƯ BẢY HIỀN " +
+                        "(149 Bành Văn Trân, P.7, Q.Tân Bình)" +
+                        "Phòng mới xây đang hoàn thiện nội thất ( Phòng Mới 100%), tháng 9 dọn vào ở. " +
+                        "Cho những ai thích căn phòng sang trọng ở khu vực trầm lắng yên bình.",
+                        R.drawable.ic_launcher_foreground));
+        arrMotelNews.add(new MotelNews("Phòng Đủ tiện nghi QUẬN 7",
+                "Cần cho thuê phòng đẹp, đủ tiện nghi cơ bản xách vali vào là ở " +
+                        "1.Nhà rất đẹp và ngay mặt dường hẻm xe hơi " +
+                        "-WC riêng " +
+                        "2. Phòng trang bị đầy đủ tiện nghi, nước nóng năng lượng mặt trời, giường, tủ, " +
+                        "máy lạnh, diện tích từ 20m2- 25m2 tuỳ lớn nhỏ",
+                        R.drawable.ic_message_black_24px));
+        arrMotelNews.add(new MotelNews("Hoàng hoa thám còn hải phòng to co ban công",
+                "Con hai phòng to co ban công " +
+                        "Giơ giấc tư do " +
+                        "Loi đi riêng " +
+                        "Khu an ninh sạch sẽ " +
+                        "Sat chợ hoang hoa thám " +
+                        "Co the ở do 3-5n " +
+                        "Uu tiên nhân viên van phòng.hộ gia đình " +
+                        "Ai co nhu câu liên hê kim",
+                        R.drawable.ic_location_black_24px));
+        motelNewsAdapter.notifyDataSetChanged();
     }
+
 
 }
